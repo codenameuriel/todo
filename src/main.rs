0ab37241846b608c7f5037ab09a728ef9b6127fb
Uriel Rodriguez <codenameuriel28@gmail.com>
2023-02-19 10:29:13 -0500

    create todo file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::fs::File;
+use std::io::ErrorKind;
+
 fn main() {
+    // mock user input
+    let todo = "item one on todo";
+
+    // open a file, if it does not exist, create it
+    // then write to it
+    // handle errors
+
+    let todo_file = File::open("todo.txt").unwrap_or_else(|e| {
+        if e.kind() == ErrorKind::NotFound {
+            File::create("todo.txt").expect("Problem creating todo.txt...")
+        } else {
+            panic!("Problem opening todo.txt...");
+        }
+    });
+
     
 }

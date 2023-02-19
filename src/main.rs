126e58a5b2592a10a00bce9902bec9f93189fb20
Uriel Rodriguez <codenameuriel28@gmail.com>
2023-02-19 10:54:01 -0500

    use builder to open/create file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::fs::File;
-use std::io::ErrorKind;
+use std::fs::OpenOptions;
+// use std::io::prelude::*;
+// use std::io::ErrorKind;
 
 fn main() {
     // mock user input
@@ -9,13 +10,17 @@ fn main() {
     // then write to it
     // handle errors
 
-    let todo_file = File::open("todo.txt").unwrap_or_else(|e| {
-        if e.kind() == ErrorKind::NotFound {
-            File::create("todo.txt").expect("Problem creating todo.txt...")
-        } else {
-            panic!("Problem opening todo.txt...");
-        }
-    });
+    // let todo_file = File::open("todo.txt").unwrap_or_else(|e| {
+    //     if e.kind() == ErrorKind::NotFound {
+    //         File::create("todo.txt").expect("Problem creating todo.txt...")
+    //     } else {
+    //         panic!("Problem opening todo.txt...");
+    //     }
+    // });
 
-    
+    // builder pattern
+    let todo_file = OpenOptions::new()
+        .create(true) // create the file if it doesn't exist
+        .append(true) // don't overwrite existing content
+        .open("todo.txt").unwrap();
 }

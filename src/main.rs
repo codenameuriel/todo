7e8ed2a7d39e60d5f335c7de904d950813fb13fb
Uriel Rodriguez <codenameuriel28@gmail.com>
2023-02-19 20:56:57 -0500

    collect cli args and write to file

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,18 @@
 use std::fs::OpenOptions;
-// use std::io::prelude::*;
-// use std::io::ErrorKind;
+use std::io::prelude::*;
+use std::env;
 
 fn main() {
-    // mock user input
-    let todo = "item one on todo";
-
-    // open a file, if it does not exist, create it
-    // then write to it
-    // handle errors
-
-    // let todo_file = File::open("todo.txt").unwrap_or_else(|e| {
-    //     if e.kind() == ErrorKind::NotFound {
-    //         File::create("todo.txt").expect("Problem creating todo.txt...")
-    //     } else {
-    //         panic!("Problem opening todo.txt...");
-    //     }
-    // });
-
+    let mut args = env::args();
+    args.next().unwrap();
+    let todo = args.next().unwrap();
+    
     // builder pattern
-    let todo_file = OpenOptions::new()
+    let mut todo_file = OpenOptions::new()
         .create(true) // create the file if it doesn't exist
         .append(true) // don't overwrite existing content
         .open("todo.txt").unwrap();
+
+    todo_file.write_all(todo.as_bytes()).unwrap();
+    todo_file.flush().unwrap();
 }
